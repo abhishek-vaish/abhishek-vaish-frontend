8496eb5923e4b59c863cfb173e0477d1844ca1bc
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2021-06-14 11:41:13 +0530

    create Home.js file

---
+++ src/container/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
