86b430378170d0c7e015e58ebfaf67938c894ca3
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2021-06-16 08:17:47 +0530

    create seperate ThreeCircle component

---
+++ src/components/ThreeCircle/ThreeCircle.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ThreeCircle.css";
+
+const ThreeCircle = () => {
+  return (
+    <div className="threecircle">
+      <div className="icons__circles">
+        <div className="circle circle-1" />
+        <div className="circle circle-2-update" />
+        <div className="circle circle-3" />
+      </div>
+    </div>
+  );
+};
+
+export default ThreeCircle;
