29b2190826a78597352ce17c04c1bd97c32d2fb7
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2021-06-16 08:02:57 +0530

    add SideToggle NavBar and Dashboard components

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./Home.css";
+import SideToggle from "../components/SideToggle/SideToggle";
+import NavBar from "../components/NavBar/NavBar";
+import Dashboard from "../components/Dashboard/Dashboard";
 
 const Home = () => {
   return (
     <div className="Home">
-      <div>
-        <h1>Hello World</h1>
+      <div className="sidetoggle">
+        <SideToggle />
+      </div>
+      <div className="navBar">
+        <NavBar />
+      </div>
+      <div className="dashboard">
+        <Dashboard />
       </div>
     </div>
   );
