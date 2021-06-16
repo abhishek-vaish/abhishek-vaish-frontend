2ee7dee5b75f777c64c2c829d1f5673ef3ca6122
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2021-06-16 12:04:46 +0530

    create three circle components

--- src/components/ThreeCircle/ThreeCircle.js
+++ src/components/ThreeCircle/ThreeCircle.js
@@ -5,9 +5,9 @@ const ThreeCircle = () => {
   return (
     <div className="threecircle">
       <div className="icons__circles">
-        <div className="circle circle-1" />
-        <div className="circle circle-2-update" />
-        <div className="circle circle-3" />
+        <div className="tcircle circle-1" />
+        <div className="tcircle circle-2-update" />
+        <div className="tcircle circle-3" />
       </div>
     </div>
   );
